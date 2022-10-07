60259418bf44b72b9759fe696d72e4886fb28cc0
Lucas Rodrigues <rodrigges@iMac-Pro.local>
2022-10-07 14:33:04 -0300

    creating admin dashboard elements

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import { deleteDoc, doc } from 'firebase/firestore';
+import React, { useContext } from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import { EventContext } from '../context/EventProvider';
+import convertDateAndTime from '../helpers/convertDateAndTime';
+import { db } from '../services/firebase';
 
 export default function AdminDashboard() {
+  const {
+    events,
+    setEvents,
+  } = useContext(EventContext);
+
+  function handleDelete({ target: { id } }) {
+    deleteDoc(doc(db, 'events', id))
+      .then(() => {
+        setEvents(events.filter((el) => el.id !== id));
+      });
+  }
+
   return (
-    <div>AdminDashboard</div>
+    <div>
+      <Header />
+      {events.length > 0 && (
+        events.map(({ id, event: { location, date, time } }) => (
+          <div>
+            <p>{`${location} (${convertDateAndTime(date, time)})`}</p>
+            <button
+              className="del-button-event"
+              type="button"
+              id={id}
+              onClick={handleDelete}
+            >
+              <i className="fa-solid fa-trash" id={id} />
+            </button>
+          </div>
+        ))
+      ) }
+      <Footer />
+    </div>
   );
 }
