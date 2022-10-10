d43237f57708df7f51e2aab367a787e60d760b25
Lucas Rodrigues <rodrigges@iMac-Pro.local>
2022-10-09 21:21:48 -0300

    :white_check_mark: userRegister link test at Login.test.jsx

--- src/tests/Login.test.jsx
+++ src/tests/Login.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
