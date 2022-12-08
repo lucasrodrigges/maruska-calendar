a28704f50b6e2eda760194a36976d0adc42fb5c4
lucasrodrigges <rodriggeslucas@gmail.com>
2022-12-07 23:02:59 -0300

    :sparkles: corrigindo conflitos da branch 0.1.1 com a main

--- frontend/src/components/EventCards.jsx
+++ frontend/src/components/EventCards.jsx
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import React, { useContext } from 'react';
-=======
-import React, { useContext, useEffect, useState } from 'react';
-import { deleteDoc, doc } from 'firebase/firestore';
-import { db } from '../services/firebase';
->>>>>>> d75a555f2ffbad5e01158869670d37113898eb12
 import convertDateAndTime from '../helpers/convertDateAndTime';
 import { months } from '../helpers/data';
 import sadUnicorny from '../images/sadUnicorny.png';
@@ -12,7 +6,6 @@ import { EventContext } from '../context/EventProvider';
 
 export default function EventCards() {
   const {
-<<<<<<< HEAD
     events,
   } = useContext(EventContext);
 
@@ -20,38 +13,6 @@ export default function EventCards() {
     <div className="calendar-general-container">
 
       {events.length > 0 ? events.map(({
-=======
-    toUpdate,
-    setUpdate,
-    events,
-  } = useContext(EventContext);
-
-  const [eventsClone, setEventsClone] = useState([]);
-
-  useEffect(() => {
-    const orderedEvents = events
-      .sort(({ event: { date: dateA, time: timeA } }, { event: { date: dateB, time: timeB } }) => {
-        if (dateA === dateB) {
-          return +timeA.split(':').join('') - +timeB.split(':').join('');
-        }
-        return +dateA.split('-').join('') - +dateB.split('-').join('');
-      });
-
-    setEventsClone([...orderedEvents]);
-  }, [events]);
-
-  async function handleDelete({ target: { id } }) {
-    await deleteDoc(doc(db, 'events', id));
-    setUpdate(!toUpdate);
-  }
-
-  return (
-    <div className="calendar-general-container">
-      {/* <div>
-        <h2 className="event-cards-title">Shows agendados</h2>
-      </div> */}
-      {eventsClone.length > 0 ? eventsClone.map(({
->>>>>>> d75a555f2ffbad5e01158869670d37113898eb12
         id, event: {
           location, date, time, members, description,
         },
@@ -62,11 +23,7 @@ export default function EventCards() {
           <p className="event-items">Banda:</p>
           <ul className="ul-band">
             {Object.keys(members).map((el) => (
-<<<<<<< HEAD
               <div key={members[el].name}>
-=======
-              <div key={members.name}>
->>>>>>> d75a555f2ffbad5e01158869670d37113898eb12
                 <li className="ul-item">{members[el].name}</li>
               </div>
             ))}
@@ -74,22 +31,6 @@ export default function EventCards() {
           {description && (
             <span className="event-description">{`Observação: ${description}`}</span>
           )}
-<<<<<<< HEAD
-=======
-          <div className="event-buttons-container">
-            {/* <button className="del-button-event " type="button">
-              <i className="fa-solid fa-check-to-slot" />
-            </button> */}
-            <button
-              className="del-button-event"
-              type="button"
-              id={id}
-              onClick={handleDelete}
-            >
-              <i className="fa-solid fa-trash" id={id} />
-            </button>
-          </div>
->>>>>>> d75a555f2ffbad5e01158869670d37113898eb12
         </div>
       )) : (
         <div>
