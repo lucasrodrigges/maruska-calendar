84a19f4bd21dc52cfc05c5eba1d13b17389a8bef
Lucas Rodrigues <rodrigges@pop-os.localdomain>
2022-12-13 01:01:10 -0300

    :recycle: mudando timezone mysql

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -7,9 +7,9 @@ module.exports = {
   host: process.env.MYSQLHOST,
   port: process.env.MYSQLPORT,
   dialect: 'mysql',
-  // timezone: 'America/Fortaleza',
+  timezone: '+03:00',
   dialectOptions: {
-    // useUTC: false,
+    // useUTC: false
   },
   define: {
     timestamps: true,
